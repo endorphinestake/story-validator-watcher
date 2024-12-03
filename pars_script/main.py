b3dde07abeaf942117069f8f7b72e88b2f0d8ecf
karlo <93879680+karloinv@users.noreply.github.com>
2024-12-03 21:37:30 +0200

    Create main.py

---
+++ pars_script/main.py
@@ -0,0 +1,21 @@
+import schedule
+from parse.parse import Parse
+from parse.get_subprocess_link import get_command
+import subprocess
+
+def main():
+    tokens = Parse().run()
+    if tokens:
+        command = get_command(tokens)
+        try:
+            print('Run subprocess: ',' '.join(command))
+            subprocess.run(command)
+        except Exception as ex:
+            print(ex)
+
+if __name__ == '__main__':
+    main()
+    schedule.every(1).minutes.do(main)
+    while True:
+        schedule.run_pending()
+    
