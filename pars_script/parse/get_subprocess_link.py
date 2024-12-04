6afc940c182fc22382cf359a0ae1ead0b50e40f8
karlo <93879680+karloinv@users.noreply.github.com>
2024-12-04 22:11:54 +0200

    Update get_subprocess_link.py

--- pars_script/parse/get_subprocess_link.py
+++ pars_script/parse/get_subprocess_link.py
@@ -1,6 +1,8 @@
 def get_command(tokens):
     command = [
         "cosmos-validator-watcher",
+        "--node", "https://story-testnet.rpc.kjnodes.com:443"
+        "--node", "https://story-testnet-rpc.itrocket.net:443"
         "--node", "https://story-testnet-rpc.blockhub.id:443"
     ]
     for token in tokens:
